44323928e79df579bdc2738a3af5af6a43468449
Cihan <work@Cihans-MacBook-Pro-3.local>
2018-07-09 03:08:39 +0300

    Challenge 1 done. Fix a bug that caused the transition to get stuck via keyup

---
+++ 01 - JavaScript Drum Kit/js/main.js
@@ -0,0 +1,32 @@
+function playSound(ev) {
+    const keyCode = ev.keyCode;
+
+    if (!ev.keyCode) {
+        return;
+    }
+
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${ev.keyCode}"]`);
+
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0; // this is to rewind
+    audio.play();
+}
+
+function removeTransition(ev) {
+    if (ev.propertyName !== 'transform') return;
+    ev.target.classList.remove('playing');
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+document.addEventListener("keydown", playSound);
+
+document.addEventListener('keyup', function (e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(key && key.classList.contains('playing')) key.classList.remove('playing');
+  });
+
